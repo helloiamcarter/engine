80d829a4d60c3adadde883b19092614243509913
helloiamcarter <great.carterisgreat@gmail.com>
2014-10-22 18:57:41 -0500

    First commit of Indexer

---
+++ Indexer.java
@@ -0,0 +1,17 @@
+
+package org.uiowa.cs2820.engine;
+
+import org.uiowa.cs2820.engine.Database;
+import org.uiowa.cs2820.engine.Field;
+
+public class Indexer {
+	Indexer(id) {
+		return;
+	    }
+	add(Field newf) { 
+		}
+
+	close() {
+		}
+
+	}
