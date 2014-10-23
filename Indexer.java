783d2b4a0ec99db64105bda9b7995ebd6bdf2eed
helloiamcarter <great.carterisgreat@gmail.com>
2014-10-23 11:23:01 -0500

    New commit - almost complete class

--- Indexer.java
+++ Indexer.java
@@ -6,9 +6,13 @@ import org.uiowa.cs2820.engine.Field;
 
 public class Indexer {
 	Indexer(id) {
-		return;
+		String path = id;
+		Database db = new Database();
 	    }
 	add(Field newf) { 
+		String fieldName = newf.getFieldName();
+		String fieldValue = newf.getFieldValue();
+		db.add(path, fieldValue);
 		}
 
 	close() {
